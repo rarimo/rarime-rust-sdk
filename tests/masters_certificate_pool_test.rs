a0ddb1e847cffc47d153db83d5b9d90df11fcfa3
ivanlele <ivanlele402@gmail.com>
2025-03-20 13:32:37 +0200

    extent the masters certificate pool

--- tests/masters_certificate_pool_test.rs
+++ tests/masters_certificate_pool_test.rs
@@ -2,15 +2,37 @@
 mod tests {
     use std::{fs::File, io::Read};
 
-    use rarime_rust_sdk::{masters_certificate_pool::MastersCertificatePool, rfc::rfc5280};
+    use rarime_rust_sdk::{
+        masters_certificate_pool::MastersCertificatePool, passport::Passport, rfc::rfc5280,
+    };
 
     #[test]
     fn test_certificate_pool() {
-        let mut file =
+        let mut passport_file = File::open("tests/assets/test_passport.json")
+            .expect("failed to open test passport data");
+
+        let mut passport_data = String::new();
+        passport_file
+            .read_to_string(&mut passport_data)
+            .expect("failed to read test passport data");
+
+        let passport: Passport =
+            serde_json::from_str(&passport_data).expect("failed to parse passport");
+
+        let signed_data = passport.parse_signed_data().expect("failed to parse sod");
+
+        let asn1::Choice1::ChoiceA(slave) = signed_data
+            .certificates
+            .expect("at least one certificate should be present")
+            .last()
+            .unwrap();
+
+        let mut masters_pems_file =
             File::open("tests/assets/masters.pem").expect("failed to open certificate masters");
 
         let mut masters_pems_data = String::new();
-        file.read_to_string(&mut masters_pems_data)
+        masters_pems_file
+            .read_to_string(&mut masters_pems_data)
             .expect("failed to read certificate masters data");
 
         let masters_pems = pem::parse_many(&masters_pems_data).expect("failed to parse raw pems");
@@ -25,5 +47,7 @@ mod tests {
 
         let mut masters_certificate_pool = MastersCertificatePool::new();
         masters_certificate_pool.add_masters(master_certificates);
+
+        masters_certificate_pool.find_master(slave);
     }
 }
