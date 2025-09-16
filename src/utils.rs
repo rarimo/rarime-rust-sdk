8bf5c0ef5e1589c4167a03dc402b15db1231c3fd
Bogdan Guba <guba.bogd@@gmail.com>
2025-09-16 18:00:06 +0300

    Adding comments to code
    Signed-off-by: Bogdan Guba <guba.bogd@@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ pub mod rarime_utils {
     use babyjubjub_rs::new_key;
     use num_bigint::ToBigInt;
 
-    // NewBJJSecretKey generates a new secret key for the Baby JubJub curve.
+    // GenerateBJJSecretKey generates a new secret key for the Baby JubJub curve.
     pub fn generate_bjj_secret_key() -> Result<Vec<u8>, RarimeError> {
         let private_key = new_key();
         let scalar = private_key.scalar_key();
@@ -26,20 +26,16 @@ mod tests {
 
     #[test]
     fn test_generate_bjj_secret_key_uniqueness() {
-        // Указываем, сколько раз будем вызывать функцию
         let num_keys_to_generate = 5;
 
         let mut generated_keys = Vec::new();
 
-        // Генерируем несколько ключей и сохраняем их
         for _ in 0..num_keys_to_generate {
             let key = generate_bjj_secret_key().expect("Failed to generate a key");
             generated_keys.push(key);
         }
         println!("{:?}", generated_keys);
 
-        // Чтобы убедиться, что тесты работают,
-        // вы можете добавить проверку на уникальность, как мы делали ранее
         for i in 0..num_keys_to_generate {
             for j in (i + 1)..num_keys_to_generate {
                 assert_ne!(generated_keys[i], generated_keys[j], "Keys are not unique!");
