9b17e2c5b33951de1a121d1fc15b0e46831c3248
Bogdan Guba <guba.bogd@@gmail.com>
2025-09-16 13:57:05 +0300

    Add Cert and trees
    Signed-off-by: Bogdan Guba <guba.bogd@@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@ pub mod passport;
 pub mod rfc;
 
 mod base64;
+mod owned_cert;
+mod treap_tree;
 
+use ::base64::DecodeError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -16,4 +19,16 @@ pub enum RarimeError {
     RSAError(#[from] rsa::errors::Error),
     #[error("unsupported signature algorithm")]
     UnsupportedSignatureAlgorithm,
+    #[error("X509 error: {0}")]
+    X509Error(String),
+    #[error("PEM error: {0}")]
+    PemError(String),
+    #[error("No certificates found")]
+    NoCertificatesFound,
+    #[error("UTF-8 error: {0}")]
+    UTF8Error(#[from] std::str::Utf8Error),
+    #[error("decoding error: {0}")]
+    DecodeError(#[from] DecodeError),
+    #[error("Der error: {0}")]
+    DerError(String),
 }
