d69beb5c27507d23673f7f39ffa811ec1348582e
Bogdan Guba <guba.bogd@@gmail.com>
2025-09-18 11:29:20 +0300

    feat: Add BJJ secret keygen with Go SDK compatibility
    Signed-off-by: Bogdan Guba <guba.bogd@@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,15 @@
 pub mod rarime_utils {
     use crate::RarimeError;
     use babyjubjub_rs::new_key;
-    use num_bigint::ToBigInt;
 
     // GenerateBJJSecretKey generates a new secret key for the Baby JubJub curve.
     pub fn generate_bjj_secret_key() -> Result<Vec<u8>, RarimeError> {
         let private_key = new_key();
         let scalar = private_key.scalar_key();
 
-        let big_int_scalar = scalar.to_bigint().unwrap();
+        let (_, scalar_bytes) = scalar.to_bytes_be();
 
-        let (_, scalar_bytes) = big_int_scalar.to_bytes_be();
-
-        Ok(scalar_bytes)
+        return Ok(scalar_bytes);
     }
 
     pub fn generate_aa_challenge(data: &[u8]) -> Result<Vec<u8>, RarimeError> {
