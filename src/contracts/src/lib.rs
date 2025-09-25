775d0a7399bb760f4300821835442a836ba7a008
Bogdan Guba <guba.bogd@@gmail.com>
2025-09-25 17:50:16 +0300

    feat: add config for development and testing or  release
    Signed-off-by: Bogdan Guba <guba.bogd@@gmail.com>

--- src/contracts/src/lib.rs
+++ src/contracts/src/lib.rs
@@ -1,7 +1,15 @@
-use crate::StateKeeper::getPassportInfoReturn;
-use alloy::{primitives::Address, providers::ProviderBuilder, sol};
-use anyhow::Error;
-use std::str::FromStr;
+mod state_keeper;
+
+use alloy::sol;
+#[cfg(debug_assertions)]
+pub(crate) const RPC_URL: &str = "https://rpc.evm.mainnet.rarimo.com";
+#[cfg(debug_assertions)]
+pub(crate) const STATE_KEEPER_CONTRACT_ADDRESS: &str = "0x9EDADB216C1971cf0343b8C687cF76E7102584DB";
+
+#[cfg(not(debug_assertions))]
+pub(crate) const RPC_URL: &str = "https://l2.rarimo.com";
+#[cfg(not(debug_assertions))]
+pub(crate) const STATE_KEEPER_CONTRACT_ADDRESS: &str = "0x61aa5b68D811884dA4FEC2De4a7AA0464df166E1";
 
 sol!(
     #[sol(rpc)]
@@ -9,16 +17,4 @@ sol!(
     StateKeeper,
     "src/abi/StateKeeper.json"
 );
-
-pub async fn get_passport_info(passport_key: [u8; 32]) -> Result<getPassportInfoReturn, Error> {
-    let provider =
-        ProviderBuilder::new().connect_http("https://rpc.evm.mainnet.rarimo.com".parse()?);
-
-    let contract_address = Address::from_str("0x9EDADB216C1971cf0343b8C687cF76E7102584DB")?;
-
-    let contract = StateKeeper::new(contract_address, &provider);
-
-    let result = contract.getPassportInfo(passport_key.into()).call().await?;
-
-    Ok(result)
-}
+pub use state_keeper::get_passport_info;
---
+++ src/contracts/src/state_keeper.rs
@@ -0,0 +1,19 @@
+use crate::{RPC_URL, STATE_KEEPER_CONTRACT_ADDRESS, StateKeeper};
+use alloy::primitives::Address;
+use alloy::providers::ProviderBuilder;
+use anyhow::Error;
+use std::str::FromStr;
+
+pub async fn get_passport_info(
+    passport_key: [u8; 32],
+) -> Result<StateKeeper::getPassportInfoReturn, Error> {
+    let provider = ProviderBuilder::new().connect_http(RPC_URL.parse()?);
+
+    let contract_address = Address::from_str(STATE_KEEPER_CONTRACT_ADDRESS)?;
+
+    let contract = StateKeeper::new(contract_address, &provider);
+
+    let result = contract.getPassportInfo(passport_key.into()).call().await?;
+
+    Ok(result)
+}
