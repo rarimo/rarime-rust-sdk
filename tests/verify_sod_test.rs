298c46e966d78a8b040bbcb8e03fd7161d9cc0ca
Bogdan Guba <guba.bogd@gmail.com>
2025-10-21 16:12:04 +0300

    upd: update test
    Signed-off-by: Bogdan Guba <guba.bogd@gmail.com>

--- tests/verify_sod_test.rs
+++ tests/verify_sod_test.rs
@@ -4,30 +4,36 @@ mod tests {
     use base64::engine::general_purpose::STANDARD;
     use rarime_rust_sdk::{
         Rarime, RarimeAPIConfiguration, RarimeConfiguration, RarimeContractsConfiguration,
-        RarimePassport, RarimeUserConfiguration,
+        RarimePassport, RarimeUserConfiguration, RarimeUtils,
     };
     use serde_json::Value;
     use std::fs;
 
     #[tokio::test]
     async fn test_verify_sod() {
-        let json_string = fs::read_to_string("./tests/assets/passports/id_card.json").unwrap();
+        let json_string =
+            fs::read_to_string("./tests/assets/passports/IVAN_LELE_doc.json").unwrap();
         let json_value: Value = serde_json::from_str(&json_string).unwrap();
 
         let rarime_config = RarimeConfiguration {
             contracts_configuration: RarimeContractsConfiguration {
                 state_keeper_contract_address: "0x9EDADB216C1971cf0343b8C687cF76E7102584DB"
                     .to_string(),
-                register_contract_address: "".to_string(),
+                register_contract_address: "0xd63782478CA40b587785700Ce49248775398b045".to_string(),
             },
             api_configuration: RarimeAPIConfiguration {
                 json_rpc_evm_url: "https://rpc.evm.mainnet.rarimo.com".to_string(),
                 rarime_api_url: "https://api.orgs.app.stage.rarime.com".to_string(),
             },
             user_configuration: RarimeUserConfiguration {
-                user_private_key: Some([0; 32]),
+                user_private_key: Some(RarimeUtils::generate_bjj_private_key().unwrap()),
             },
         };
+
+        dbg!(hex::encode(
+            rarime_config.user_configuration.user_private_key.unwrap()
+        ));
+
         let mut rarime = Rarime::new(rarime_config);
 
         let passport = RarimePassport {
