011bbe1c5e3d4870cede80aac71f4841ab8ff0d2
Bogdan Guba <guba.bogd@gmail.com>
2025-10-25 15:17:36 +0300

    feat: add convert fun to utils
    Signed-off-by: Bogdan Guba <guba.bogd@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -148,3 +148,12 @@ pub fn convert_asn1_to_pem(asn1_block: &ASN1Block) -> Result<String, RarimeError
 
     Ok(pem_string)
 }
+
+pub fn vec_u8_to_u8_32(vec: &Vec<u8>) -> Result<[u8; 32], RarimeError> {
+    let result: [u8; 32] = vec.as_slice().try_into().map_err(|_| {
+        crate::errors::RarimeError::GetProfileKeyError(
+            "Private key must be 32 bytes in length.".to_string(),
+        )
+    })?;
+    return Ok(result);
+}
