f87ab7e6e42bfa5bb2b285ea9ba1951583cbf55f
Bogdan Guba <guba.bogd@gmail.com>
2025-11-06 11:06:26 +0200

    feat: add encoding user payload
    Signed-off-by: Bogdan Guba <guba.bogd@gmail.com>

--- src/contracts/src/call_data_builder.rs
+++ src/contracts/src/call_data_builder.rs
@@ -1,11 +1,64 @@
 use crate::{ContractsError, IdCardVoting, RegistrationSimple};
+use alloy::dyn_abi::DynSolValue;
+use alloy::primitives::U256;
 use alloy::sol_types::SolCall;
+use std::str::FromStr;
 
+pub struct UserData {
+    pub nullifier: String,
+    pub citizenship: String,
+    pub identity_creation_timestamp: String,
+}
+
+pub struct UserPayloadInputs {
+    pub proposal_id: String,
+    pub vote: Vec<String>,
+    pub user_data: UserData,
+}
 pub struct CallDataBuilder {}
 
 impl CallDataBuilder {
     pub fn new() -> Self {
         Self {}
+    }
+
+    pub fn encode_user_payload(
+        &self,
+        inputs: UserPayloadInputs,
+    ) -> Result<Vec<u8>, ContractsError> {
+        let user_payload_value = DynSolValue::Tuple(vec![
+            DynSolValue::Uint(U256::from_str(inputs.proposal_id.as_str())?, 256),
+            DynSolValue::Array(
+                inputs
+                    .vote
+                    .iter()
+                    .map(|v| {
+                        let val = U256::from_str(v.as_str())?;
+                        Ok(DynSolValue::Uint(val, 256))
+                    })
+                    .collect::<Result<Vec<_>, ContractsError>>()?,
+            ),
+            DynSolValue::CustomStruct {
+                name: "user_data".to_string(),
+                prop_names: vec![
+                    "nullifier".to_string(),
+                    "citizenship".to_string(),
+                    "identityCreationTimestamp".to_string(),
+                ],
+                tuple: vec![
+                    DynSolValue::Uint(U256::from_str(inputs.user_data.nullifier.as_str())?, 256),
+                    DynSolValue::Uint(U256::from_str(inputs.user_data.citizenship.as_str())?, 256),
+                    DynSolValue::Uint(
+                        U256::from_str(inputs.user_data.identity_creation_timestamp.as_str())?,
+                        256,
+                    ),
+                ],
+            },
+        ]);
+
+        let encoded: Vec<u8> = user_payload_value.abi_encode();
+
+        return Ok(encoded);
     }
 
     pub fn build_noir_lite_register_call_data(
