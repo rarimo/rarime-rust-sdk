7e35f9145f8f123f2f88f5f0784eb391c2a483f1
Bogdan Guba <guba.bogd@gmail.com>
2025-11-06 11:10:52 +0200

    upd: update tests
    Signed-off-by: Bogdan Guba <guba.bogd@gmail.com>

--- tests/get_polls_data_test.rs
+++ tests/get_polls_data_test.rs
@@ -6,7 +6,7 @@ mod tests {
     };
 
     #[tokio::test]
-    async fn get_polls_data_test_contracts() {
+    async fn get_polls_data_test_ipfs() {
         let freedomtool_config = FreedomtoolConfiguration {
             contracts_configuration: FreedomtoolContractsConfiguration {
                 proposals_state_address: "0x4C61d7454653720DAb9e26Ca25dc7B8a5cf7065b".to_string(),
@@ -21,34 +21,13 @@ mod tests {
 
         let proposal_id: String = "208".to_string();
 
-        let proposal_data = freedomtool
+        let proposal_data_contract = freedomtool
             .get_polls_data_contract(proposal_id)
             .await
             .unwrap();
-        dbg!(&proposal_data);
-    }
+        dbg!(&proposal_data_contract);
 
-    #[tokio::test]
-    async fn get_polls_data_test_ipfs() {
-        let freedomtool_config = FreedomtoolConfiguration {
-            contracts_configuration: FreedomtoolContractsConfiguration {
-                proposals_state_address: "0x4C61d7454653720DAb9e26Ca25dc7B8a5cf7065b".to_string(),
-            },
-            api_configuration: FreedomtoolAPIConfiguration {
-                voting_rpc_url: "https://rpc.qtestnet.org".to_string(),
-                ipfs_url: "https://ipfs.rarimo.com".to_string(),
-            },
-        };
-
-        let rarime = Freedomtool::new(freedomtool_config);
-
-        let proposal_id: String = "208".to_string();
-
-        let proposal_id: String = "208".to_string();
-
-        let proposal_data_contract = rarime.get_polls_data_contract(proposal_id).await.unwrap();
-
-        let proposal_data = rarime
+        let proposal_data = freedomtool
             .get_polls_data_ipfs(&proposal_data_contract.config.description)
             .await
             .unwrap();
