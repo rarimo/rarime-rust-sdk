bbf250e0ba4b6363995969d546d387768979bd6d
Bogdan Guba <guba.bogd@gmail.com>
2025-11-14 14:53:06 +0200

    upd: setup new binding generation
    Signed-off-by: Bogdan Guba <guba.bogd@gmail.com>

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
     let udl_file = "./rarime_rust_sdk.udl";
-    //
-    // println!("cargo:rerun-if-changed={}", udl_file);
-    //
-    // uniffi_build::generate_scaffolding_for_crate(udl_file, "rarime_rust_sdk")
-    //     .expect("Error generating scaffolding");
+
+    println!("cargo:rerun-if-changed={}", udl_file);
+
+    uniffi_build::generate_scaffolding_for_crate(udl_file, "rarime_rust_sdk")
+        .expect("Error generating scaffolding");
 }
