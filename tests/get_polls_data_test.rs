a62a4887d3b5466986ff95abc501b28eec8a4ed2
Bogdan Guba <guba.bogd@gmail.com>
2025-11-17 13:23:32 +0200

    upd: update tests
    Signed-off-by: Bogdan Guba <guba.bogd@gmail.com>

--- tests/get_polls_data_test.rs
+++ tests/get_polls_data_test.rs
@@ -6,7 +6,7 @@ mod tests {
     };
 
     #[tokio::test]
-    async fn get_polls_data_test_ipfs() {
+    async fn get_poll_data_test_ipfs() {
         let freedomtool_config = FreedomtoolConfiguration {
             contracts_configuration: FreedomtoolContractsConfiguration {
                 proposals_state_address: "0x4C61d7454653720DAb9e26Ca25dc7B8a5cf7065b".to_string(),
@@ -22,16 +22,8 @@ mod tests {
 
         let proposal_id: String = "214".to_string();
 
-        let proposal_data_contract = freedomtool
-            .get_polls_data_contract(proposal_id)
-            .await
-            .unwrap();
-        dbg!(&proposal_data_contract);
+        let poll_data = freedomtool.get_proposal_data(proposal_id).await.unwrap();
 
-        let proposal_data = freedomtool
-            .get_polls_data_ipfs(proposal_data_contract.config.description)
-            .await
-            .unwrap();
-        dbg!(&proposal_data);
+        dbg!(&poll_data);
     }
 }
