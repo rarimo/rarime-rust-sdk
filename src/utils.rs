046f924c09a485a0926c6fbe3f8cd520d272edcb
Bogdan Guba <guba.bogd@gmail.com>
2025-11-19 11:44:39 +0200

    upd: delete separate rarimo utils module
    Signed-off-by: Bogdan Guba <guba.bogd@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -10,65 +10,6 @@ use poseidon_rs::{Fr, FrRepr};
 use simple_asn1::{ASN1Block, to_der};
 use std::io::Cursor;
 
-pub mod rarime_utils {
-    use crate::RarimeError;
-    use crate::utils::poseidon_hash_32_bytes;
-    use babyjubjub_rs::new_key;
-    use ff::{PrimeField, PrimeFieldRepr};
-    use num_bigint::BigInt;
-
-    // GenerateBJJSecretKey generates a new secret key for the Baby JubJub curve.
-    pub fn generate_bjj_private_key() -> Result<[u8; 32], RarimeError> {
-        let private_key = new_key();
-        let scalar = private_key.scalar_key();
-        let (_, scalar_bytes) = scalar.to_bytes_be();
-
-        let fixed_bytes: [u8; 32] = scalar_bytes
-            .try_into()
-            .map_err(|_| RarimeError::GeneratePrivateKeyError)?;
-
-        Ok(fixed_bytes)
-    }
-
-    pub fn get_profile_key(private_key: &[u8; 32]) -> Result<[u8; 32], RarimeError> {
-        let scalar_int = BigInt::from_bytes_be(num_bigint::Sign::Plus, private_key);
-
-        let b8 = babyjubjub_rs::Point {
-            x: babyjubjub_rs::Fr::from_str(
-                "5299619240641551281634865583518297030282874472190772894086521144482721001553",
-            )
-            .ok_or(RarimeError::SetupGeneratorPointError(
-                "Failed to init Generator point X".to_string(),
-            ))?,
-            y: babyjubjub_rs::Fr::from_str(
-                "16950150798460657717958625567821834550301663161624707787222815936182638968203",
-            )
-            .ok_or(RarimeError::SetupGeneratorPointError(
-                "Failed to init Generator point Y".to_string(),
-            ))?,
-        };
-        let pub_point = b8.mul_scalar(&scalar_int);
-        let mut x_raw_bytes = Vec::new();
-        let x_raw = pub_point.x.into_repr();
-        x_raw.write_be(&mut x_raw_bytes).map_err(|e| {
-            RarimeError::SetupGeneratorPointError(format!("Error converting repr to byte: {}", e))
-        })?;
-
-        let x_big_int = BigInt::from_bytes_be(num_bigint::Sign::Plus, &x_raw_bytes);
-
-        let mut y_raw_bytes = Vec::new();
-        let y_raw = pub_point.y.into_repr();
-        y_raw.write_be(&mut y_raw_bytes).map_err(|e| {
-            RarimeError::SetupGeneratorPointError(format!("Error converting repr to byte: {}", e))
-        })?;
-
-        let y_big_int = BigInt::from_bytes_be(num_bigint::Sign::Plus, &y_raw_bytes);
-
-        let profile_key = poseidon_hash_32_bytes(&vec![x_big_int, y_big_int])?;
-        Ok(profile_key)
-    }
-}
-
 pub fn big_int_to_32_bytes(num: &BigInt) -> [u8; 32] {
     let mut out = [0u8; 32];
 
